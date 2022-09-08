6430bb6bac60e318187f75c598290fc99df0c6e5
Phillip Logan <philo3194@gmail.com>
2022-09-08 22:11:06 +0000

    first draft

--- static/script.js
+++ static/script.js
@@ -14,7 +14,27 @@ document.getElementById("submit").addEventListener("click", function (e) {
     })
       .then((res) => res.json())
       .then((json) => {
+        if (navigator.clipboard) document.getElementById("copy").style.display = "flex";
         document.getElementById("shawt-link").value = `https://shawt.link/${json.link_id}`;
       });
   }
+});
+
+document.getElementById("copy").addEventListener("click", function () {
+  // let copyText = document.getElementById("shawt-link");
+  // copyText.select();
+  // copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // if (navigator.clipboard) navigator.clipboard.writeText(copyText.value);
+
+  var text = document.getElementById("shawt-link").value;
+
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("Text copied to clipboard");
+    })
+    .catch((err) => {
+      alert("Error in copying text: " + err);
+    });
 });
